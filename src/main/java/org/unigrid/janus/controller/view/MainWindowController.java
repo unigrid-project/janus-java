797d517a540b04bb1269bc632ab1db5def2d3a15
Greg Kourupes <greg@unigrid.org>
2022-01-03 13:22:36 -0800

    Functional mockup

--- src/main/java/org/unigrid/janus/controller/view/MainWindowController.java
+++ src/main/java/org/unigrid/janus/controller/view/MainWindowController.java
@@ -21,11 +21,12 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.VBox;
 import org.unigrid.janus.model.service.DebugService;
 import org.unigrid.janus.model.service.RPCService;
 import org.unigrid.janus.model.service.WindowService;
-import org.unigrid.janus.model.rpc.entity.NewAddress;
+// import org.unigrid.janus.model.rpc.entity.NewAddress;
 
 public class MainWindowController implements Initializable {
 	private static DebugService debug = new DebugService();
@@ -40,16 +41,69 @@ public class MainWindowController implements Initializable {
 	@FXML
 	private void onGetAddress(MouseEvent event) {
 		debug.log("Get address clicked!");
-		debug.log(rpc.callToJson(new NewAddress.Request("Wilcokat007")));
+		// debug.log(rpc.callToJson(new NewAddress.Request("Wilcokat007")));
 	}
 
 	@FXML
 	private void onWalletTap(MouseEvent event) {
-
+		try {
+			ToggleButton btnWallet = (ToggleButton) window.lookup("btnWallet");
+			ToggleButton btnTransactions = (ToggleButton) window.lookup("btnTransactions");
+			ToggleButton btnNodes = (ToggleButton) window.lookup("btnNodes");
+			btnTransactions.setSelected(false);
+			btnNodes.setSelected(false);
+			btnWallet.setSelected(true);
+			VBox pnlWallet = (VBox) window.lookup("pnlWallet");
+			VBox pnlTransactions = (VBox) window.lookup("pnlTransactions");
+			VBox pnlNodes = (VBox) window.lookup("pnlNodes");
+			pnlTransactions.setVisible(false);
+			pnlNodes.setVisible(false);
+			pnlWallet.setVisible(true);
+			debug.log("Wallet clicked!");
+		} catch (Exception e) {
+			debug.log(String.format("ERROR: (wallet click) %s", e.getMessage()));
+		}
 	}
 
 	@FXML
 	private void onTransactionsTap(MouseEvent event) {
+		try {
+			ToggleButton btnWallet = (ToggleButton) window.lookup("btnWallet");
+			ToggleButton btnTransactions = (ToggleButton) window.lookup("btnTransactions");
+			ToggleButton btnNodes = (ToggleButton) window.lookup("btnNodes");
+			btnNodes.setSelected(false);
+			btnWallet.setSelected(false);
+			btnTransactions.setSelected(true);
+			VBox pnlWallet = (VBox) window.lookup("pnlWallet");
+			VBox pnlTransactions = (VBox) window.lookup("pnlTransactions");
+			VBox pnlNodes = (VBox) window.lookup("pnlNodes");
+			pnlWallet.setVisible(false);
+			pnlNodes.setVisible(false);
+			pnlTransactions.setVisible(true);
+			debug.log("Transactions clicked!");
+		} catch (Exception e) {
+			debug.log(String.format("ERROR: (transactions click) %s", e.getMessage()));
+		}
+	}
 
+	@FXML
+	private void onNodesTap(MouseEvent event) {
+		try {
+			ToggleButton btnWallet = (ToggleButton) window.lookup("btnWallet");
+			ToggleButton btnTransactions = (ToggleButton) window.lookup("btnTransactions");
+			ToggleButton btnNodes = (ToggleButton) window.lookup("btnNodes");
+			btnWallet.setSelected(false);
+			btnTransactions.setSelected(false);
+			btnNodes.setSelected(true);
+			VBox pnlWallet = (VBox) window.lookup("pnlWallet");
+			VBox pnlTransactions = (VBox) window.lookup("pnlTransactions");
+			VBox pnlNodes = (VBox) window.lookup("pnlNodes");
+			pnlWallet.setVisible(false);
+			pnlTransactions.setVisible(false);
+			pnlNodes.setVisible(true);
+			debug.log("Nodes clicked!");
+		} catch (Exception e) {
+			debug.log(String.format("ERROR: (nodes click) %s", e.getMessage()));
+		}
 	}
 }
