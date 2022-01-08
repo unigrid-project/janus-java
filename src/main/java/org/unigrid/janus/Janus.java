04d7ea8b4fee37681325930143053a590f4a1409
gkourupes <gkourupes@yahoo.com>
2022-01-08 06:01:42 -0800

    Transactions (#4)
    * Added RPC calls
    
    * Added temporary debug listview
    
    * Begin GUI mockup
    
    * Functional mockup
    
    * Hotfix toggle button hover highlight
    
    * Showing balance and money supply in window title
    
    * WIP wallet transactions
    
    * Added Transactions
    
    * Hotfix merge issues
    
    * Hotfix merge issues
    
    * Hotfix merge issues
    
    Co-authored-by: Greg Kourupes <greg@unigrid.org>

--- src/main/java/org/unigrid/janus/Janus.java
+++ src/main/java/org/unigrid/janus/Janus.java
@@ -95,11 +95,7 @@ public class Janus extends BaseApplication {
 
 			debug.log(rpc.callToJson(new DataDirectory.Request()));
 
-<<<<<<< HEAD
 			debug.log(rpc.callToJson(new ListTransactions.Request(0, 10)));
-=======
-			debug.log(rpc.callToJson(new ListTransactions.Request()));
->>>>>>> master
 
 			debug.log(rpc.callToJson(new ListAddressGroupings.Request()));
 
