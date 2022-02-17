4622e15f306e89c65bf613ec43eadfcb42c50cfd
Evan Green <gevan73@gmail.com>
2022-02-17 14:34:59 +0100

    Gridnode: created a basic setup and view for the gridnode screen. Jsch is available for a VPS connection although this will be fully implemented in a later release.

--- src/main/java/org/unigrid/janus/controller/view/NodesController.java
+++ src/main/java/org/unigrid/janus/controller/view/NodesController.java
@@ -12,13 +12,27 @@
 
 	You should have received an addended copy of the GNU Affero General Public License with this program.
 	If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/janus-java>.
-*/
-
+ */
 package org.unigrid.janus.controller.view;
 
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.ChannelShell;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import org.unigrid.janus.model.service.DebugService;
 import org.unigrid.janus.model.service.RPCService;
 import org.unigrid.janus.model.service.WindowService;
@@ -28,8 +42,113 @@ public class NodesController implements Initializable {
 	private static RPCService rpc = new RPCService();
 	private static WindowService window = new WindowService();
 
+	@FXML
+	private TextField vpsPassword;
+	@FXML
+	private TextField vpsAddress;
+	@FXML
+	private TextArea vpsOutput;
+	@FXML
+	private VBox vpsConect;
+
+	private String serverResponse;
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		/* Empty on purpose */
+		vpsOutput.textProperty().addListener(new ChangeListener() {
+			public void changed(ObservableValue ov, Object oldValue, Object newValue) {
+				vpsOutput.setScrollTop(Double.MAX_VALUE);    //top
+				//vpsOutput.setScrollTop(Double.MIN_VALUE);   //down
+			}
+		});
+		vpsConect.setVisible(false);
+	}
+	
+	@FXML
+	public void connectToVps() throws Exception {
+		int port = 22;
+		Session session = null;
+		Channel channel = null;
+		ChannelShell shell = null;
+		try {
+			session = new JSch().getSession("root", vpsAddress.getText(), port);
+			session.setPassword(vpsPassword.getText());
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.connect();
+			channel = session.openChannel("shell");
+
+			OutputStream inputstream_for_the_channel = channel.getOutputStream();
+			PrintStream commander = new PrintStream(inputstream_for_the_channel, true);
+
+			channel.setOutputStream(System.out, true);
+
+			channel.connect();
+
+			commander.println("ls -la");
+			commander.close();
+			//commander.println("bash -ic \"$(wget -4qO- -o- raw.githubusercontent.com/unigrid-project/gridnode-setup/master/unigridd.sh)\" ; source ~/.bashrc");
+			//commander.println("ls -la");
+			//commander.println("exit");
+			readChannelOutput(channel);
+			commander.close();
+
+			do {
+				Thread.sleep(1000);
+			} while (!channel.isEOF());
+
+			session.disconnect();
+
+		} finally {
+			if (session != null) {
+				session.disconnect();
+			}
+			if (channel != null) {
+				channel.disconnect();
+			}
+		}
+	}
+
+	private void readChannelOutput(Channel channel) {
+
+		byte[] buffer = new byte[1024];
+
+		try {
+			InputStream in = channel.getInputStream();
+			String line = "";
+			while (true) {
+				while (in.available() > 0) {
+					int i = in.read(buffer, 0, 1024);
+					if (i < 0) {
+						break;
+					}
+					line = new String(buffer, 0, i);
+					if(line.contains("txhash:")){
+						debug.log("ENTER COMMAND HERE");
+					}
+					updateTerminal(line);
+				}
+
+				if (line.contains("logout")) {
+					break;
+				}
+
+				if (channel.isClosed()) {
+					break;
+				}
+				try {
+					Thread.sleep(1000);
+				} catch (Exception ee) {
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error while reading channel output: " + e);
+		}
+
+	}
+
+	private void updateTerminal(String str) {
+		serverResponse = serverResponse + str;
+		vpsOutput.setText(serverResponse);
+		vpsOutput.setScrollTop(Double.MAX_VALUE);
 	}
 }
