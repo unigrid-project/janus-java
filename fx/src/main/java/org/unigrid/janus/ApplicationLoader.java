d0933b6ce5e661fc8f1d5733d0d1024dbd17caab
Adam Waldenberg <adam@unigrid.org>
2022-07-07 22:22:39 +0200

    Update license information
    Janus is now dual-licensed between UGD Software AB and The Unigrid
    Foundation.

--- fx/src/main/java/org/unigrid/janus/ApplicationLoader.java
+++ fx/src/main/java/org/unigrid/janus/ApplicationLoader.java
@@ -1,6 +1,6 @@
 /*
     The Janus Wallet
-    Copyright © 2021-2022 The Unigrid Foundation
+    Copyright © 2021-2022 The Unigrid Foundation, UGD Software AB
 
     This program is free software: you can redistribute it and/or modify it under the terms of the
     addended GNU Affero General Public License as published by the Free Software Foundation, version 3
