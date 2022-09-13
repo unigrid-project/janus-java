0895c2783490cae3516a20b707bfeaaddeefd578
dekm <gevan73@gmail.com>
2022-09-13 16:27:18 +0200

    created package for window bar component

---
+++ fx/src/test/java/org/unigrid/janus/controller/component/WindowBarControllerTest.java
@@ -0,0 +1,12 @@
+package org.unigrid.janus.controller.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.weld.junit5.WeldJunit5Extension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.unigrid.janus.FXApplicationTest;
+
+@Slf4j
+@ExtendWith(WeldJunit5Extension.class)
+public class WindowBarControllerTest extends FXApplicationTest {
+
+}
