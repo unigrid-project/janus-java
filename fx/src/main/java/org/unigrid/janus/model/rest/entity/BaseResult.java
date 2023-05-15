5239185f6b8d333f122c9b41305eb09b1b6013a1
Fim-84 <marcus.stenberg@gmail.com>
2023-05-15 15:01:07 +0200

    MS: Started work on rest comunication with cosmos based chain. import web3j using a shaded artifact to fix package problems

---
+++ fx/src/main/java/org/unigrid/janus/model/rest/entity/BaseResult.java
@@ -0,0 +1,50 @@
+/*
+	The Janus Wallet
+	Copyright © 2021-2022 The Unigrid Foundation, UGD Software AB
+
+	This program is free software: you can redistribute it and/or modify it under the terms of the
+	addended GNU Affero General Public License as published by the Free Software Foundation, version 3
+	of the License (see COPYING and COPYING.addendum).
+
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+	even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU Affero General Public License for more details.
+
+	You should have received an addended copy of the GNU Affero General Public License with this program.
+	If not, see <http://www.gnu.org/licenses/> and <https://github.com/unigrid-project/janus-java>.
+ */
+
+package org.unigrid.janus.model.rest.entity;
+
+import jakarta.json.bind.annotation.JsonbProperty;
+import lombok.Data;
+
+@Data
+public class BaseResult<T> {
+	private T result;
+	private ErrorResult error;
+	private String id;
+
+	protected BaseResult() {
+		/* Can only be instantiated during inheritance */
+	}
+
+	@Data
+	public static class ErrorResult {
+		private String error;
+		private int code;
+		private String message;
+		private Details[] detailses;
+	}
+
+	@Data
+	public static class Details {
+		@JsonbProperty("type_url")
+		private String typeUrl;
+		private String value;
+	}
+
+	public boolean hasError() {
+		return (error != null);
+	}
+}
